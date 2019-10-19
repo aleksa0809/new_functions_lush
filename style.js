2b4b277692b6751da019d20262559e4abdb24d68
aleksandra sagitova <aleksandrasagitova@aleksandras-MacBook-Pro.local>
2019-10-19 11:36:37 +0200

    new code

--- style.js
+++ style.js
@@ -1,15 +1,15 @@
 document.body.style.background = 'lightgray';
 
 
-function convertCase(changeCase) {
+function ConvertCase(changeCase) {
     if (changeCase == "upper") {
-        document.form1.value = document.forms1.value.toUpperCase();
+        document.form1.elements.data1.value = document.form1.elements.data1.value.toUpperCase();
 
-        document.form2.value = document.forms2.value.toUpperCase();
+        document.form1.elements.data2.value = document.form1.elements.data2.value.toUpperCase();
     } else {
-        document.form1.value = document.forms1.value.toLowerCase();
+        document.form1.elements.data1.value = document.form1.elements.data1.value.toLowerCase();
 
-        document.form2.value = document.forms2.value.toLowerCase();
+        document.form1.elements.data2.value = document.form1.elements.data2.value.toLowerCase();
     }
 
 }
